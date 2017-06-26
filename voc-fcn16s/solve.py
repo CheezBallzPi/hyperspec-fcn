f2321ed24b65efc3690404d41a3999b4f4fb8e81
Jackie Chen <cheezballz3.14@gmail.com>
2017-06-26 15:38:00 +1000

    Add VOC nets

---
+++ voc-fcn16s/solve.py
@@ -0,0 +1,32 @@
+import caffe
+import tools.surgery, tools.score
+
+import numpy as np
+import os
+import sys
+
+try:
+    import setproctitle
+    setproctitle.setproctitle(os.path.basename(os.getcwd()))
+except:
+    pass
+
+weights = '../voc-fcn32s/snapshot/train_iter_24000.caffemodel'
+
+# init
+caffe.set_device(0)
+caffe.set_mode_gpu()
+
+solver = caffe.SGDSolver('solver.prototxt')
+solver.net.copy_from(weights)
+
+# surgeries
+interp_layers = [k for k in solver.net.params.keys() if 'up' in k]
+tools.surgery.interp(solver.net, interp_layers)
+
+# scoring
+val = np.loadtxt('../data/VOC/seg11valid.txt', dtype=str)
+
+for _ in range(25):
+    solver.step(4000)
+    tools.score.seg_tests(solver, False, val, layer='score')

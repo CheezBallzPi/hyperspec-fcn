458742cc2d5d4104f14eb7b78ca8f251f0509e85
Jackie Chen <cheezballz3.14@gmail.com>
2017-08-03 09:36:08 -0400

    Fix formatting

--- hsfcn8s-atonce/tools/mat_helper.py
+++ hsfcn8s-atonce/tools/mat_helper.py
@@ -6,23 +6,25 @@ import scipy.io as spio
 
 from PIL import Image
 
+
 class hypermat:
+
     def __init__(self, data_path, label_path, classes):
-    	# data_path is /path/to/pavia
-		immat = spio.loadmat(data_path)
-		lbmat = spio.loadmat(label_path)
-		
+        # data_path is /path/to/pavia
+        immat = spio.loadmat(data_path)
+        lbmat = spio.loadmat(label_path)
+
         lbmat = lbmat[np.newaxis, ...]
-    	# self.classes = ['Asphalt','Meadows','Gravel','Trees',
-    	# 				'Painted metal sheets','Bare Soil',
-    	# 				'Bitumen','Self-Blocking Bricks','Shadows']
+        # self.classes = ['Asphalt','Meadows','Gravel','Trees',
+        #               'Painted metal sheets','Bare Soil',
+        #               'Bitumen','Self-Blocking Bricks','Shadows']
         self.classes = classes
     
     def load_band(self, band):
-    	return immat[:,:,band]
-    	
+        return immat[:,:,band]
+        
     def load_image(self):
-    	return immat
-    	
+        return immat
+        
     def load_labels(self):
-    	return lbmat
+        return lbmat

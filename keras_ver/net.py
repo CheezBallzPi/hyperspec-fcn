68e2ea306a9165e8ed9809d2199a81e71995fef6
Jackie Chen <cheezballz3.14@gmail.com>
2017-12-30 14:39:57 -0500

    Finish net

--- keras_ver/net.py
+++ keras_ver/net.py
@@ -6,23 +6,22 @@ def net():
     model = Sequential()
 
     # Inputs are (27,27,103)
-
     # Conv
-    model.add(L.Conv3D(32, (32,4,4), strides=(32,1,1), activation='relu', input_shape=(103,27,27)))
-    model.add(L.MaxPooling2D())
+    model.add(L.Conv3D(32,32,4,4, activation='relu', input_shape=(103,27,27,1)))
+    model.add(L.MaxPooling3D(pool_size=(1,2,2)))
 
-    model.add(L.Conv3D(64, (32,5,5), strides=(32,1,1), activation='relu'))
-    model.add(L.MaxPooling2D())
+    model.add(L.Conv3D(64,32,5,5, activation='relu'))
+    model.add(L.MaxPooling3D(pool_size=(1,2,2)))
     model.add(L.Dropout(.5))
 
-    model.add(L.Conv3D(128, (32,4,4), strides=(32,1,1), activation='relu'))
+    model.add(L.Conv3D(128,32,4,4, activation='relu'))
     model.add(L.Dropout(.5))
 
     # Fully Connected
-    model.add(Flatten())
-    model.add(Dense(128, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(9, activation='softmax'))
+    model.add(L.Flatten())
+    model.add(L.Dense(128, activation='relu'))
+    model.add(L.Dropout(.5))
+    model.add(L.Dense(9, activation='softmax'))
 
     # Loss
     model.compile(optimizer='adagrad',
